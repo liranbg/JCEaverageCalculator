c9ef4351e7b92d3891189d0da6a6f46371d0df93
Sagi Dayan <sagidayan@gmail.com>
2014-06-26 20:31:48 +0300

    Init Translate stuff

---
+++ jce_he.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he_IL">
+<context>
+    <name>MainScreen</name>
+    <message>
+        <location filename="main/mainscreen.ui" line="20"/>
+        <source>JCE Avg Calculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="86"/>
+        <location filename="main/mainscreen.ui" line="193"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="177"/>
+        <source>Keep login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="234"/>
+        <source>&lt;color=#000000&gt;Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="260"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="306"/>
+        <source>Courses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="343"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Get your grades&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="346"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="353"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Clear table&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="356"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="380"/>
+        <source>Average:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="428"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="474"/>
+        <source>Semester</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="497"/>
+        <source>Year</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="530"/>
+        <source>From</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="549"/>
+        <source>To</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="655"/>
+        <source>Only influence Courses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="683"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="707"/>
+        <source>Credits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="712"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.ui" line="717"/>
+        <source>How-To</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.cpp" line="77"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main/mainscreen.cpp" line="77"/>
+        <source>Not Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
